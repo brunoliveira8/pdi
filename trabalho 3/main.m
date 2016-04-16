8456261e128f42cf420098c5e0b3fe17c3a367a9
Bruno Oliveira <tarcisiobrunooliveira@gmail.com>
2016-04-16 08:24:44 -0300

    Start to code the main file

--- trabalho 3/main.m
+++ trabalho 3/main.m
@@ -1,2 +1,16 @@
 %Trabalho 3
 
+img = imread('lena_grayscale.png');
+
+
+
+amount = 100;
+img_noises = zeros(amount);
+
+for i = 1:amount
+  img_noises(i) = imnoise(img,'gaussian');
+end
+
+
+figure;
+imshow(img)
